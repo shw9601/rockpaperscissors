e4b75d693dba24dc6821c490a96b36a55e4aa258
shw9601 <shw9601@rit.edu>
2024-06-28 17:06:47 -0400

    Change function to button UI and on-screen display Add buttons to pick rock/paper/scissors. Add functionality to display who won on page and update score tally on page.

--- rps.js
+++ rps.js
@@ -13,36 +13,40 @@ function getComputerChoice() {
     }
 }
 
-
-function getHumanChoice() {
-    let choice = prompt("Enter rock, paper or scissors: ").toLowerCase();
-    return choice;
-}
-
-
 // Scores initiated to 0
 let humanScore = 0;
 let computerScore = 0;
 
+const whoWon = document.querySelector("#whoWon");
+const scoreboard = document.querySelector("#scoreboard");
+scoreboard.textContent = "Human score: " + humanScore + "  -  Computer Score: " + computerScore;
+
+
+
+function updateScore() {
+    scoreboard.textContent = "Human score: " + humanScore + "  -  Computer Score: " + computerScore;
+}
 
 function lose(human, computer) {
     computerScore += 1;
-    console.log("You lose, " + computer + " beats " + human + ".");
+    whoWon.textContent = "You lose, " + computer + " beats " + human + ".";
+    updateScore();
 }
 
 function win(human, computer) {
     humanScore += 1;
-    console.log("You win, " + human + " beats " + computer + ".");
+    whoWon.textContent = "You win, " + human + " beats " + computer + ".";
+    updateScore();
 }
 
 function tie(human) {
-    console.log("Tie game: both you and the computer chose " + human + ".");
+    whoWon.textContent = "Tie game: both you and the computer chose " + human + ".";
 }
 
 
-function playRound() {
+function playRound(humanChoice) {
+
     let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
 
     if (computerChoice == "rock") {
         if (humanChoice == "scissors") {
@@ -77,32 +81,27 @@ function playRound() {
             tie(humanChoice);
         }
     }
-}
 
+    if (humanScore == 5) {
 
-
-function playGame() {
-    humanScore = 0;
-    computerScore = 0;
-
-    let gameCounter = 1;
-
-    while (gameCounter <= 5) {
-        playRound();
-        gameCounter++;
     }
+    if (computerScore == 5) {
 
-    console.log("The score is....");
-    console.log("Human: " + humanScore);
-    console.log("Computer: " + computerScore);
-
-    if (humanScore > computerScore) {
-        console.log("You win");
-    }
-    else if (computerScore > humanScore) {
-        console.log("The computer wins");
-    }
-    else {
-        console.log("Tie score");
     }
 }
+
+
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+    playRound("rock");
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+    playRound("paper");
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+    playRound("scissors");
+})
